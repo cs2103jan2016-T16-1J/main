111b950c94ae43bfc0539440d2370a6b052d705a
unknown <immortal243@hotmail.com>
2016-03-02 13:10:14 +0800

    Initial GUI window

---
+++ src/MainWindow.java
@@ -0,0 +1,47 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class MainWindow {
+
+	private JFrame frame;
+	private int WINDOW_X = 100;
+	private int WINDOW_Y = 100;
+	private int WINDOW_WIDTH = 650;
+	private int WINDOW_HEIGHT = 600;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
