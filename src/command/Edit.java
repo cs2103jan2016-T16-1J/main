da5bcde8a62cfc2edecf138fa039d0cf75ca8aa1
Reem Razak <reemrazak@gmail.com>
2016-03-07 02:57:39 +0800

    Created Edit Command

---
+++ src/command/Edit.java
@@ -0,0 +1,101 @@
+package command;
+
+import main.Event;
+import main.State;
+
+/**
+ * Edit class must be instantiated two Events- the original Event and the new edited Event
+ * @author Reem
+ *
+ */
+public class Edit implements Command{
+	Event originalEvent;
+	Event editedEvent;
+	State completeState;
+
+	/**
+	 * Edit Event constructor
+	 * @param originalEvent the original event located in completeState
+	 * @param editedEvent new information in the form of an Event object that will replace the original event
+	 */
+	public Edit(Event originalEvent, Event editedEvent){
+		this.originalEvent = originalEvent;
+		this.editedEvent = editedEvent;
+		
+	}
+
+	/**
+	 * Inherited from the Command interface
+	 * execute will edit the event from its corresponding list based on status
+	 * @param completeState the state of all the tasks in the program
+	 */
+	public State execute(State completeState){
+		this.completeState = completeState;
+		int index = findIndexOfEvent();
+		
+		if(index == -1){
+			completeState.setStatusMessage(State.MESSAGE_EVENT_NOT_FOUND);
+			return completeState;
+		}
+		
+		switch (originalEvent.getStatus()){
+		case COMPLETE:
+			editInCompletedEventList(index);
+			break;
+		case INCOMPLETE:
+			editInIncompletedEventList(index);
+			break;
+		case FLOATING:
+			editInFloatingEventList(index);
+			break;
+		}
+		
+		return completeState;
+	}
+	
+	/**
+	 * Replaces the event if it is in the completed list
+	 * @param index the index of the event
+	 */
+	public void editInCompletedEventList(int index){
+		completeState.completedTasks.set(index, editedEvent);
+	}
+	
+	/**
+	 * Replaces the event if it is in the incompleted list
+	 * @param index the index of the event
+	 */
+	public void editInIncompletedEventList(int index){
+		completeState.incompletedTasks.set(index, editedEvent);
+	}
+	
+	/**
+	 * Replaces the event if it is in the floating list
+	 * @param index the index of the event
+	 */
+	public void editInFloatingEventList(int index){
+		completeState.floatingTasks.set(index, editedEvent);
+	}
+	
+	/**
+	 * returns the index of the event so that the new event can be replaced at the old event's index
+	 * @return
+	 */
+	public int findIndexOfEvent(){
+		int index = 0;
+		
+		switch (originalEvent.getStatus()){
+		case COMPLETE:
+			completeState.completedTasks.indexOf(originalEvent);
+			break;
+		case INCOMPLETE:
+			completeState.incompletedTasks.indexOf(originalEvent);
+			break;
+		case FLOATING:
+			completeState.floatingTasks.indexOf(originalEvent);
+			break;
+		}
+		
+		return index;
+	}
+}
--- src/main/State.java
+++ src/main/State.java
@@ -7,15 +7,17 @@ import main.Event;;
 
 public class State {
 
+	public static final String MESSAGE_EVENT_NOT_FOUND = "The request event was not found";
 	public ArrayList<Event> completedTasks;
 	public ArrayList<Event> incompletedTasks;
 	public ArrayList<Event> floatingTasks;
 	
 	public ArrayList<Event> displayedTasks;
 	Stack<Event> eventHistory;
-
 	
 	public Event selectedTask;
+	
+	public String statusMessage;
 
 	/**
 	 * CompleteState constructor initializes all Event Lists
@@ -25,6 +27,15 @@ public class State {
 		incompletedTasks = new ArrayList<Event>();
 		floatingTasks = new ArrayList<Event>();
 		displayedTasks = new ArrayList<Event>();
+		statusMessage = new String();
 
+	}
+	
+	public void setStatusMessage(String statusMessage){
+		this.statusMessage = statusMessage;
+	}
+	
+	public String getStatusMessage(){
+		return this.statusMessage;
 	}
 }
