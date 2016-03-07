f74012e78c6678559fc53368e1f52268ab07fe43
Claudia Zhou <claudiazhou@221-205.priv27.nus.edu.sg>
2016-03-08 01:11:47 +0800

    Modify State and storage

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -44,6 +44,7 @@ import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -80,8 +81,9 @@ public class MainWindow {
 
 	/**
 	 * Launch the application.
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		
 		
@@ -101,6 +103,9 @@ public class MainWindow {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
+		storage.Storage.createFile();
+		
 		testNewEvent.setName("TEST EVENT NAME");
 		testNewEvent.setDescription("This is a test event created in main");
 		testNewEvent.setLocation("Supahotfire's house");
