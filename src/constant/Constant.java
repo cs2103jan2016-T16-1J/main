c38fe301e3b58d4f6d740d7694ab40142d071458
Reem Razak <reemrazak@gmail.com>
2016-03-27 16:52:25 +0800

    Changed Constant Status/Category

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -14,12 +14,12 @@ public class Constant {
 	public static final String CATEGORY_DEADLINE = "DEADLINE";
 	public static final String CATEGORY_FLOATING = "FLOATING";
 	public static final String CATEGORY_UNDETERMINED = "UNDETERMINED";
-	public static final Category CATEGORY_NULL = null;
+	public static final Category CATEGORY_NULL = Category.NULL;
 	
 	public static final String STATUS_INCOMPLETE = "EVENT";
 	public static final String STATUS_COMPLETE = "DEADLINE";
 	public static final String STATUS_FLOATING = "FLOATING";
 	public static final String STATUS_BLOCKED = "BLOCKED";
 	public static final String STATUS_OVERDUE = "OVERDUE";
-	public static final Status STATUS_NULL = null;
+	public static final Status STATUS_NULL = Status.NULL;
 }
