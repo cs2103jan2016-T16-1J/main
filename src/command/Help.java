92bec4429bb4e3d3e76a33e04f83568580a834c4
Claudia Zhou <claudiazhou@222-129.priv27.nus.edu.sg>
2016-03-28 23:48:31 +0800

    Added help command

---
+++ src/command/Help.java
@@ -0,0 +1,22 @@
+package command;
+
+import java.io.IOException;
+
+import json.JSONException;
+import main.State;
+
+public class Help implements Command {
+
+	@Override
+	public State execute(State completeState) throws IOException, JSONException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatedDisplayedEvents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
