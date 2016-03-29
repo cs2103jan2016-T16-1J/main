f87a3e2c5e9f4307f09b92899f9caa1ee7ea2bfe
Claudia Zhou <claudiazhou@186-123.priv23.nus.edu.sg>
2016-03-29 19:23:50 +0800

    Export command

---
+++ src/command/Export.java
@@ -0,0 +1,21 @@
+package command;
+
+import java.io.IOException;
+
+import json.JSONException;
+import main.Event;
+import main.State;
+
+public class Export implements Command {
+
+	public Export(Event event) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public State execute(State completeState) throws IOException, JSONException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
