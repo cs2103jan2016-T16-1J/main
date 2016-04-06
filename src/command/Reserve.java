3650e63e6df49ae372d401e201527ab24454dc1b
Reem Razak <reemrazak@gmail.com>
2016-04-06 15:17:49 +0800

    Remove EventToEvent method in Add, created methods in Reserve

--- src/command/Reserve.java
+++ src/command/Reserve.java
@@ -16,7 +16,7 @@ import storage.Storage;
  * @author Reem
  *
  */
-public class Add implements Command{
+public class Reserve implements Command{
 
 	GenericEvent modifiedEvent;
 	State completeState;
@@ -25,7 +25,7 @@ public class Add implements Command{
 	 * Add class constructor
 	 * @param modifiedEvent the event that will be added
 	 */
-	public Add(GenericEvent modifiedEvent){
+	public Reserve(GenericEvent modifiedEvent){
 		this.modifiedEvent = modifiedEvent;
 	}
 
@@ -45,23 +45,14 @@ public class Add implements Command{
 		}
 		
 		switch (modifiedEvent.getStatus()){
-		case COMPLETE:
-			addToCompleteList();
-			break;
-		case INCOMPLETE:
-			addToIncompleteList();
-			break;
-			
-		case UNDETERMINED:
-			addToUndeterminedList();
-			break;
-			
-			/*This will be fulfilled by Reseve Command
-		case UNDETERMINED:
-			addToFloatingList();
-			break;*/
-			
-		}
+			case UNDETERMINED:
+				addToUndeterminedList();
+				break;
+			default:
+				//completeState.setStatusMessage(State.MESSAGE_INVALID_RESERVED);
+				break;
+							
+			}
 		
 		/*to select the previously added or reserved event*/
 		completeState.clearSelections();
@@ -83,29 +74,18 @@ public class Add implements Command{
 		
 		return true;
 	}
-	/**
-	 * adds the given task to the completed list in State
-	 */
-	public void addToCompleteList(){
-		completeState.completedEvents.add((Event) modifiedEvent);
-	}
-
-	/**
-	 * adds the given task to the incomplete list in State
-	 */
-	public void addToIncompleteList(){
-		completeState.incompletedEvents.add((Event) modifiedEvent);
-	}
-
 	public void addToUndeterminedList(){
 		
 		if(modifiedEvent.getCategory() == Category.FLOATING){
-			completeState.undeterminedEvents.add((ReservedEvent) modifiedEvent);
+			completeState.setStatusMessage(State.MESSAGE_ATTEMPTED_ADD_WITH_RESERVE);
 		} else{
 			completeState.reservedEvents.add((ReservedEvent)modifiedEvent);
+			completeState.undeterminedEvents.add((ReservedEvent)modifiedEvent);
+
 		}
 	}
-	
+
+
 	/**
 	 * updates the displayedEvents with new information
 	 */
